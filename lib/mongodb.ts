68772d426620e32e2b9b768fb20b4b724d7e1c10
Asif Rayhan <asifrayhan@Asifs-MacBook-Air.local>
2026-09-19 17:50:30 +0600

    updatessss

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,29 +1,36 @@
 import { MongoClient } from "mongodb";
 
-if (!process.env.MONGODB_URI) {
-  throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
-}
-
-const uri = process.env.MONGODB_URI;
-const options = {};
-
-let client: MongoClient;
-let clientPromise: Promise<MongoClient>;
-
 declare global {
   // eslint-disable-next-line no-var
   var _mongoClientPromise: Promise<MongoClient> | undefined;
 }
 
-if (process.env.NODE_ENV === "development") {
+function getClientPromise(): Promise<MongoClient> {
+  const uri = process.env.MONGODB_URI;
+  if (!uri) {
+    throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
+  }
+
+  const options = {};
+
   if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options);
+    const client = new MongoClient(uri, options);
     global._mongoClientPromise = client.connect();
   }
-  clientPromise = global._mongoClientPromise;
-} else {
-  client = new MongoClient(uri, options);
-  clientPromise = client.connect();
+  return global._mongoClientPromise;
 }
 
+const clientPromise = {
+  then(onfulfilled?: any, onrejected?: any) {
+    return getClientPromise().then(onfulfilled, onrejected);
+  },
+  catch(onrejected?: any) {
+    return getClientPromise().catch(onrejected);
+  },
+  finally(onfinally?: any) {
+    return getClientPromise().finally(onfinally);
+  },
+} as Promise<MongoClient>;
+
 export default clientPromise;
+
